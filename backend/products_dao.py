d13a530766571cf7fa24b5fefde6bef2eeb40249
Haque-Misbahul <mhaque.tuc@gmail.com>
2024-11-08 21:34:57 +0100

    wrote query

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -4,5 +4,8 @@ cnx = mysql.connector (user = 'root', password = 'password',
                        host = '127.0.0.1',
                        database = 'employees')
 
-                       
+query = " SELECT * FROM grocery_store.products"
+
+cursor.execute(query)
+
 cnx.close()
