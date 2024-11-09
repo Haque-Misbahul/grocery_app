7b3c09f2b5a2918330dbf27bd3b546eec4fc3725
Haque-Misbahul <mhaque.tuc@gmail.com>
2024-11-09 22:26:14 +0100

    updated query with join

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -6,11 +6,15 @@ cnx = mysql.connector.connect(user='root', password='macbook#94',
 
 cursor = cnx.cursor()
 
-query = "SELECT * FROM grocery_store.products"
+query = """
+    SELECT products.product_id, products.products_name, products.uom_id, uom.uom_name, products.price_per_unit
+    FROM products
+    INNER JOIN uom ON products.uom_id = uom.uom_id
+"""
 
 cursor.execute(query)
 
-for(product_id, name, uom_id, price_per_unit) in cursor:
-    print(product_id, name, uom_id, price_per_unit)
+for (product_id, name, uom_id, uom_name, price_per_unit) in cursor:
+    print(product_id, name, uom_id, uom_name, price_per_unit)
 
 cnx.close()
