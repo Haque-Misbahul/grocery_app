0642ce17e3e71d7ef904504df65a014f8e110b67
Haque-Misbahul <mhaque.tuc@gmail.com>
2024-11-11 02:19:41 +0100

    order details added

--- backend/order_dao.py
+++ backend/order_dao.py
@@ -1,6 +1,35 @@
 from datetime import datetime
 from sql_connection import get_sql_connection
 
+
+def get_order_details(connection, order_id):
+    cursor = connection.cursor()
+
+    query = "SELECT * from order_details where order_id = %s"
+
+    query = "SELECT order_details.order_id, order_details.quantity, order_details.total_price, "\
+            "products.name, products.price_per_unit FROM order_details LEFT JOIN products on " \
+            "order_details.product_id = products.product_id where order_details.order_id = %s"
+
+    data = (order_id, )
+
+    cursor.execute(query, data)
+
+    records = []
+    for (order_id, quantity, total_price, product_name, price_per_unit) in cursor:
+        records.append({
+            'order_id': order_id,
+            'quantity': quantity,
+            'total_price': total_price,
+            'product_name': product_name,
+            'price_per_unit': price_per_unit
+        })
+
+    cursor.close()
+
+    return records
+
+
 def get_all_orders(connection):
     cursor = connection.cursor()
     query = ("SELECT * FROM orders")
